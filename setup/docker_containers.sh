d790025689f479b6b83b87a349a9b80a4550a496
Zylk <rxronin77@proton.me>
2024-10-26 03:28:18 +0100

    Update and rename docker_containers.md to docker_containers.sh

--- setup/docker_containers.sh
+++ setup/docker_containers.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
+
 echo "🐧 Pulling and running Debian container..."
 sudo docker pull debian:latest
 sudo docker run -it --name debian-base debian:latest
-run_parrot.sh
 
-#!/bin/bash
 echo "🎭 Pulling and running Parrot Security container..."
 sudo docker pull parrotsec/core
 sudo docker run -it --name parrot-env parrotsec/core
-run_alpine1.sh
 
-#!/bin/bash
 echo "⛰️ Pulling and running Alpine container 1..."
 sudo docker pull alpine:latest
 sudo docker run -it --name alpine-lite1 alpine:latest
-run_alpine2.sh
 
-#!/bin/bash
 echo "⛰️ Pulling and running Alpine container 2..."
 sudo docker pull alpine:latest
 sudo docker run -it --name alpine-lite2 alpine:latest
+
